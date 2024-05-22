85730befd546c052c68ae5564d60d0f52ab815d4
Quocanh289 <160709137+Quocanh289@users.noreply.github.com>
2024-05-22 14:46:21 +0700

    Update logic.h

--- logic.h
+++ logic.h
@@ -2,8 +2,8 @@
 #define _LOGIC__H
 #define BOARD_SIZE 3
 #define EMPTY_CELL ' '
-#define O_CELL 'o'
-#define X_CELL 'x'
+#define O_CELL 'O'
+#define X_CELL 'X'
 
 struct Tictactoe {
     char board[BOARD_SIZE][BOARD_SIZE];
@@ -18,9 +18,7 @@ struct Tictactoe {
     }
     int move(int row, int column)
 {
-
-
-    // Thực hiện nước đi và chuyển đổi người chơi tiếp theo
+    // Di va nuoc tiep theo
     if (row >= 0 && row < BOARD_SIZE && column >= 0 && column < BOARD_SIZE)
         {
             board[row][column] = nextMove;
@@ -36,44 +34,42 @@ struct Tictactoe {
 }
 
 
-    char checkWinner() const {
-    // Kiểm tra hàng ngang
-
-    for (int i = 0; i < BOARD_SIZE; i++) {
-    // Khởi tạo cờ cho hàng ngang hiện tại
-    bool isRowWin = true;
-    // So sánh tất cả các ô trong hàng ngang
-    for (int j = 1; j < BOARD_SIZE; j++) {
-        if (board[i][j] == EMPTY_CELL || board[i][j] != board[i][0]) {
-            // Nếu ô không giống hoặc là ô trống, đánh dấu không phải hàng chiến thắng
-            isRowWin = false;
-            break;
+    char checkWinner() const
+{
+    // kiem tra hang ngang
+    for (int i = 0; i < BOARD_SIZE; i++)
+    {
+        bool isRowWin = true;
+        for (int j = 1; j < BOARD_SIZE; j++)
+        {
+            if (board[i][j] == EMPTY_CELL || board[i][j] != board[i][0])
+            {
+                isRowWin = false;
+                break;
+            }
+        }
+        if (isRowWin)
+        {
+            return board[i][0];
         }
     }
-    // Nếu tất cả các ô trong hàng ngang đều giống nhau và không phải là ô trống, in ra giá trị của ô đầu tiên
-    if (isRowWin) {
-        return board[i][0];
-
-    }
-}
    // kiem tra hang doc
-    for (int i = 0; i < BOARD_SIZE; i++) {
-    // Khởi tạo cờ cho hàng ngang hiện tại
-    bool isRowWin = true;
-    // So sánh tất cả các ô trong hàng ngang
-    for (int j = 1; j < BOARD_SIZE; j++) {
-        if (board[i][j] == EMPTY_CELL || board[i][j] != board[0][i]) {
-
-            isRowWin = false;
-            break;
+    for (int i = 1; i < BOARD_SIZE; i++)
+    {
+        bool isColWin = true;
+        for (int j = 0; j < BOARD_SIZE; j++)
+        {
+            if (board[j][i] == EMPTY_CELL || board[j][i] != board[0][i])
+            {
+                isColWin = false;
+                break;
+            }
+        }
+        if (isColWin)
+        {
+            return board[0][i];
         }
     }
-    // Nếu tất cả các ô trong hàng ngang đều giống nhau và không phải là ô trống, in ra giá trị của ô đầu tiên
-    if (isRowWin) {
-        return board[0][i];
-
-    }
-}
     // kiem tra duong cheo chinh
     bool isWin = true;
     for( int i =0; i<BOARD_SIZE; i++)
@@ -88,7 +84,7 @@ struct Tictactoe {
        return board[0][0];
 
     }
-    // kiemtra duong cheo phu
+    // kiem tra duong cheo phu
     bool Win = true;
     for( int i =0; i<BOARD_SIZE; i++)
     {
@@ -98,39 +94,37 @@ struct Tictactoe {
             break;
         }
     }
-    if(Win){
-        return board[0][BOARD_SIZE-1];
+    if(Win)
+        {
+            return board[0][BOARD_SIZE-1];
+        }
+    // kiem tra bang con trong hay khong
 
-    }
-//    // Kiểm tra xem còn ô trống nào không, nếu không thì là hòa
-//    for (int i = 0; i < BOARD_SIZE; i++) {
-//        for (int j = 0; j < BOARD_SIZE; j++) {
-//            if (board[i][j] == EMPTY_CELL) {
-//                return EMPTY_CELL; // Chưa kết thúc trò chơi
-//            }
-//        }
-//    }
-bool isBoardFull = true;
-for (int i = 0; i < BOARD_SIZE; i++) {
-    for (int j = 0; j < BOARD_SIZE; j++) {
-        if (board[i][j] == EMPTY_CELL) {
-            isBoardFull = false;
+    bool isBoardFull = true;
+    for (int i = 0; i < BOARD_SIZE; i++)
+    {
+        for (int j = 0; j < BOARD_SIZE; j++)
+        {
+            if (board[i][j] == EMPTY_CELL)
+            {
+                isBoardFull = false;
+                break;
+            }
+        }
+        if (!isBoardFull)
+        {
             break;
         }
     }
-    if (!isBoardFull) {
-        break;
+    if (!isBoardFull)
+    {
+        return EMPTY_CELL;
     }
-}
-// Nếu không có ô trống nào, trả về 'T' để biểu thị hòa
-if (!isBoardFull) {
-    return EMPTY_CELL;
-}
 
     return '0'; // Hòa
-}
+    }
 
-};
+    };
 
 #endif
 
